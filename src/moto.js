91d50a9bc67a245872dc5651e0a5359713ead56e
MattMNC <matteomancuso@protonmail.com>
2022-02-08 15:44:40 +0000

    SCSS cimmiting

---
+++ src/moto.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("testObj.ts");
+class Moto {
+    avvia() {
+        throw new Error("Method not implemented.");
+    }
+    guida(distanza) {
+        throw new Error("Method not implemented.");
+    }
+    getPosizione() {
+        throw new Error("Method not implemented.");
+    }
+}
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,22 @@
-function sum(x,y)
-{
-    return x+y;
+class Studente{
+
+    id:number;
+    name:String;
+
+    constructor(_id:number, _name:String)
+    {
+        this.id = _id;
+        this.name = _name;
+    }
+
+    display():void
+    {
+            console.log("studente"  + this.id);
+            
+    }   
 }
-console.log(2,3);
+
+let obj = new Studente(3, "Gianfranco");
+
+
+obj.display();
